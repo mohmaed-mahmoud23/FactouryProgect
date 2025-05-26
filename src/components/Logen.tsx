4c25d59ad13ed1d688f5160242a39cbdeccc994e
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-05-26 23:33:59 +0000

    ResponsevLohenAndRegster

--- src/components/Logen.tsx
+++ src/components/Logen.tsx
@@ -6,6 +6,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 interface IFormInput {
   email: string;
   password: string;
+  userType: "عميل" | "تاجر" | "صاحب مصنع";
 }
 
 const Logen = () => {
@@ -16,34 +17,83 @@ const Logen = () => {
   } = useForm<IFormInput>({
     resolver: yupResolver(Loginscema),
   });
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   console.log(errors);
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className=" justify-center items-center h-[220px]  gap- my-auto grid mt-30 "
+      className="w-[90%] max-w-[400px] mx-auto grid gap-5 mt-30 px-2"
     >
       <div>
         <input
           placeholder="Your Email"
-          {...register("email", { required: true, minLength: 5 })}
-          className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-md bg-transparent"
+          {...register("email")}
+          className="w-full border border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md bg-transparent xl:w-lg "
         />
         {errors?.email && (
           <p className="text-red-500">{errors.email.message}</p>
         )}
       </div>
+
       <div>
         <input
           placeholder="Your Password"
-          {...register("password", { required: true, minLength: 5 })}
-          className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-md bg-transparent"
+          type="password"
+          {...register("password")}
+          className="w-full border border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md bg-transparent xl:w-lg "
         />
         {errors?.password && (
           <p className="text-red-500">{errors.password.message}</p>
         )}
       </div>
-      <button className="border-2 outline-none w-md px-2 py-2    border-indigo-400 hover:text-white bg-transparent text-black hover:border-transparent hover:bg-gray-800 dark:text-gray-800 dark:hover:text-white">
+
+      <div className="flex flex-col gap-2">
+        <label className="text-gray-700 font-medium">اختر نوع المستخدم:</label>
+        <div className="flex flex-col sm:flex-row gap-4  xl:w-lg ">
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              value="عميل"
+              {...register("userType", {
+                required: "من فضلك اختر نوع المستخدم",
+              })}
+            />
+            عميل
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              value="تاجر"
+              {...register("userType", {
+                required: "من فضلك اختر نوع المستخدم",
+              })}
+            />
+            تاجر
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="radio"
+              value="صاحب مصنع"
+              {...register("userType", {
+                required: "من فضلك اختر نوع المستخدم",
+              })}
+            />
+            صاحب مصنع
+          </label>
+        </div>
+        {errors.userType && (
+          <p className="text-red-500">{errors.userType.message}</p>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className="w-full border-2 px-2 py-2 border-indigo-400 hover:text-white bg-transparent text-black hover:border-transparent hover:bg-gray-800 dark:text-gray-800 dark:hover:text-white"
+      >
         Register
       </button>
     </form>
--- src/components/Regester.tsx
+++ src/components/Regester.tsx
@@ -20,47 +20,47 @@ const Regester = () => {
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   console.log(errors);
   return (
-  
-     <form
-       onSubmit={handleSubmit(onSubmit)}
-       className=" justify-center items-center h-[350px]  gap-5 mx-15 my-auto grid mt-20 "
-     >
-       <div>
-         <input
-           placeholder="Your First Name"
-           className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-md bg-transparent "
-           {...register("firstName", { required: true, minLength: 5 })}
-           id="s"
-         />
-         {errors?.firstName && (
-           <p className="text-red-500">{errors.firstName.message}</p>
-         )}
-       </div>
-       <div>
-         <input
-           placeholder="Your Email"
-           {...register("email", { required: true, minLength: 5 })}
-           className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-md bg-transparent"
-         />
-         {errors?.email && (
-           <p className="text-red-500">{errors.email.message}</p>
-         )}
-       </div>
-       <div>
-         <input
-           placeholder="Your Password"
-           {...register("password", { required: true, minLength: 5 })}
-           className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-md bg-transparent"
-         />
-         {errors?.password && (
-           <p className="text-red-500">{errors.password.message}</p>
-         )}
-       </div>
-       <button className="border-2 outline-none w-md px-2 py-2    border-indigo-400 hover:text-white bg-transparent text-black hover:border-transparent hover:bg-gray-800 dark:text-gray-800 dark:hover:text-white">
-         Register
-       </button>
-     </form>
-  
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-[90%] max-w-[400px] mx-auto grid gap-5 mt-30 px-2"
+    >
+      <div>
+        <input
+          placeholder="Your First Name"
+          {...register("firstName", { required: true, minLength: 5 })}
+          className="w-full border border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md bg-transparent   "
+        />
+        {errors?.firstName && (
+          <p className="text-red-500">{errors.firstName.message}</p>
+        )}
+      </div>
+
+      <div>
+        <input
+          placeholder="Your Email"
+          {...register("email", { required: true, minLength: 5 })}
+          className="w-full border border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md bg-transparent"
+        />
+        {errors?.email && (
+          <p className="text-red-500">{errors.email.message}</p>
+        )}
+      </div>
+
+      <div>
+        <input
+          placeholder="Your Password"
+          {...register("password", { required: true, minLength: 5 })}
+          className="w-full border border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md bg-transparent xl:w-lg "
+        />
+        {errors?.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+      </div>
+
+      <button className="w-full border-2 px-2 py-2 border-indigo-400 hover:text-white bg-transparent text-black hover:border-transparent hover:bg-gray-800 dark:text-gray-800 dark:hover:text-white xl:w-lg ">
+        Register
+      </button>
+    </form>
   );
 };
 
